8b6a6b06f6a22868e1e367d788022b325e14c050
ola-dev <olamidelukman3@gmail.com>
2024-07-23 16:58:41 +0100

    fixadminjs

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -12,12 +12,14 @@ export default function Admin() {
     const [ stakeholderAddress, setStakeholderAddress ] = useState('');
     const [ stakeholderAmount, setStakeholderAmount ] = useState('');
     const [ releaseTime, setReleaseTime ] = useState('');
+    const [ loading, setLoading ] = useState();
 
     const handleCreateOrg = async () => {
+        setLoading(true);
         try {
 
             if (typeof window.ethereum !== 'undefined') {
-                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                const provider = new ethers.BrowserProvider(window.ethereum);
                 const signer = provider.getSigner();
                 const factory = new ethers.ContractFactory(
                     OrganizationToken.abi,
@@ -36,15 +38,18 @@ export default function Admin() {
         } catch (error) {
             alert("Error in handleCreateOrg: " + error.message);
             console.error("Error in handleCreateOrg:", error);
+        } finally {
+            setLoading(false)
         }
     };
 
     
     const handleAddStakeholder = async () => {
+        setLoading(true)
         try {
 
             if (typeof window.ethereum !== 'undefined') {
-                const provider = new ethers.provider.Web3Provider(window.ethereum);
+                const provider = new ethers.BrowserProvider(window.ethereum);
                 const signer = provider.getSigner();
                 const vestingContract = new ethers.Contract(vestingContractAddress, Vesting.abi, signer)
     
@@ -57,6 +62,8 @@ export default function Admin() {
         } catch (error) {
             alert("Error in handleAddStakeholder: " + error.message);
             console.error("Error in handleAddStakeholder:", error);
+        } finally {
+            setLoading(false)
         }
     };
 
@@ -65,49 +72,66 @@ export default function Admin() {
             <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
                 <h2 className="text-2xl font-bold mb-6">Register Organization</h2>
                 <input 
+                    value={orgName}
                     type="text" 
                     placeholder="Organization Name"
                     onChange={e => setOrgName(e.target.value)}
-                    className="placeholder:italic w-full mb-4 p-2 border rounded" 
+                    className="placeholder:italic w-full mb-4 p-2 border rounded"
+                    aria-label="Organization Name"
                 />
                 <input
+                    value={orgSymbol}
+                    type='text'
                     placeholder="Organization Symbol"
                     onChange={e => setOrgSymbol(e.target.value)}
                     className="placeholder:italic w-full mb-4 p-2 border rounded"
+                    aria-label="Organization Symbol"
                 />
                 <input
+                    value={intialSupply}
+                    type='text'
                     placeholder="Initial Supply"
                     onChange={e => setInitialSupply(e.target.value)}
                     className="placeholder:italic w-full mb-4 p-2 border rounded"
+                    aria-label="Initial Supply"
                 />
                 <button
                     onClick={handleCreateOrg}
-                    className="w-full px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-200"
+                    className={`w-full px-4 py-2 bg-green-500 text-white 
+                        rounded hover:bg-green-600 transition duration-200 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                    disabled={loading}
                 >
-                    Create Organization
+                    {loading ? 'Creating...' : 'Create Organization'}
                 </button>
 
                 <h2 className="text-2xl font-bold mt-8 mb-6">Add Stakeholder</h2>
                 <input
+                    value={stakeholderAddress}
+                    type='text'
                     placeholder="Stakeholder Address"
                     onChange={e => setStakeholderAddress(e.target.value)}
                     className="placeholder:italic w-full mb-4 p-2 border rounded"
                 />
                 <input
+                    type='number'
+                    value={stakeholderAmount}
                     placeholder="Amount"
                     onChange={e => setStakeholderAmount(e.target.value)}
                     className="placeholder:italic w-full mb-4 p-2 border rounded"
                 />
                 <input
+                    value={releaseTime}
+                    type='datetime-local'
                     placeholder="Release Time"
                     onChange={e => setReleaseTime(e.target.value)}
                     className="placeholder:italic w-full mb-4 p-2 border rounded"
                 />
                 <button
                     onClick={handleAddStakeholder}
-                    className="placeholder:italic w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200"
+                    className={`placeholder:italic w-full px-4 py-2 bg-blue-500 text-white
+                     rounded hover:bg-blue-600 transition duration-200 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
                 >
-                    Add Stakeholder
+                    {loading ? 'Adding...' : 'Add Stackholder'}
                 </button>
                 <div className="mt-8">
                     <Link href="/">
